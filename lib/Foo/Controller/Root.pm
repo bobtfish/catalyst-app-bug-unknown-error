74d1fe4553c9b369dcfb7a87e3b6d3cce4bca91e
t0m <bobtfish@bobtfish.net>
2009-05-05 09:19:11 +0100

    Remove the rest of COnfigLoader + parent + RenderView. Next, start inlining code from Schema::Loader into the testapp

--- lib/Foo/Controller/Root.pm
+++ lib/Foo/Controller/Root.pm
@@ -2,29 +2,9 @@ package Foo::Controller::Root;
 
 use strict;
 use warnings;
-use parent 'Catalyst::Controller';
+use base 'Catalyst::Controller';
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config->{namespace} = '';
-
-=head1 NAME
-
-Foo::Controller::Root - Root Controller for Foo
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=cut
-
-=head2 index
-
-=cut
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
@@ -38,24 +18,5 @@ sub default :Path {
     $c->response->body( 'Page not found' );
     $c->response->status(404);
 }
-
-=head2 end
-
-Attempt to render a view, if needed.
-
-=cut
-
-sub end : ActionClass('RenderView') {}
-
-=head1 AUTHOR
-
-Anders Nor Berle
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
