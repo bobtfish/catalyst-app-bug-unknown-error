2cb21dd010a8cd1736622bbb9b7c721ebf2de2ef
t0m <bobtfish@bobtfish.net>
2009-05-06 01:23:21 +0100

    More inlining

--- lib/Foo/Model/Foo.pm
+++ lib/Foo/Model/Foo.pm
@@ -1,3 +1,12 @@
+package Foo::Schema;
+
+use base qw/DBIx::Class::Schema/;
+
+use strict;
+use warnings;
+
+__PACKAGE__->load_namespaces;
+
 package Catalyst::Model::DBIC::Schema;
 
 use strict;
@@ -6,66 +15,26 @@ use warnings;
 our $VERSION = '0.23';
 
 use base qw/Catalyst::Model Class::Accessor::Fast Class::Data::Accessor/;
-use MRO::Compat;
-use mro 'c3';
-use UNIVERSAL::require;
-use Carp;
-use Data::Dumper;
-require DBIx::Class;
+#use MRO::Compat;
+#use mro 'c3';
+#use UNIVERSAL::require;
+#use Carp;
+#use Data::Dumper;
+#require DBIx::Class;
 
-__PACKAGE__->mk_classaccessor('composed_schema');
-__PACKAGE__->mk_accessors('schema');
+#__PACKAGE__->mk_classaccessor('composed_schema');
+#__PACKAGE__->mk_accessors('schema');
 
-sub new {
-    my $self = shift->next::method(@_);
+#sub new {
+#    my $self = shift->next::method(@_);
     
-    my $class = ref($self);
-    my $model_name = $class;
-    $model_name =~ s/^[\w:]+::(?:Model|M):://;
+    #my $schema_class = $self->{schema_class};
 
-    croak "->config->{schema_class} must be defined for this model"
-        unless $self->{schema_class};
+    #$schema_class->require
+    #    or croak "Cannot load schema class '$schema_class': $@";
 
-    my $schema_class = $self->{schema_class};
-
-    $schema_class->require
-        or croak "Cannot load schema class '$schema_class': $@";
-
-    if( !$self->{connect_info} ) {
-        if($schema_class->storage && $schema_class->storage->connect_info) {
-            $self->{connect_info} = $schema_class->storage->connect_info;
-        }
-        else {
-            croak "Either ->config->{connect_info} must be defined for $class"
-                  . " or $schema_class must have connect info defined on it."
-          . " Here's what we got:\n"
-          . Dumper($self);
-        }
-    }
-
-    $self->composed_schema($schema_class->compose_namespace($class));
-    $self->schema($self->composed_schema->clone);
-
-    $self->schema->storage_type($self->{storage_type})
-        if $self->{storage_type};
-
-    $self->schema->connection( 
-        ref $self->{connect_info} eq 'ARRAY' ? 
-        @{$self->{connect_info}} : 
-        $self->{connect_info}
-    );
-    
-    no strict 'refs';
-    foreach my $moniker ($self->schema->sources) {
-        my $classname = "${class}::$moniker";
-        *{"${classname}::ACCEPT_CONTEXT"} = sub {
-            shift;
-            shift->model($model_name)->resultset($moniker);
-        }
-    }
-
-    return $self;
-}
+#    return $self;
+#}
 
 package Foo::Model::Foo;
 
